9bc97eeb5a0bad2d0e49de14ec877c5390182491
Leo CP <leo011093@hotmail.com>
2017-03-29 17:10:51 -0500

    Subiendo los archivos de practicas 1, 2, 3

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Proyecto de una Libreria Pequeña implementada con phonegap
+
+Luego de haber clonado el repositorio, estar seguros de que tenemos instalado Node.js
+
+si esto es cierto entonces lo corremos con el siguiente comando:
+
+$ cordova run browser
---
+++ platforms/browser/www/js/Libro.js
@@ -0,0 +1,24 @@
+/* 
+var miLbro1 = new Object(){;
+  mi libro.isbn        = "";
+  mi libro.nombre      = "";
+  mi libro.editorial   = "";
+  mi libro.autores     = "";
+  mi libro.fecha_publi = "";
+}
+
+var miLbro1 = new Object(){;
+  mi libro["isbn          = "";
+  mi libro["nombre        = "";
+  mi libro["editorial     = "";
+  mi libro["autores       = "";
+  mi libro["fecha_publi"] = "";
+}
+*/
+ function Libro(){
+ 	this.isbn        = "";
+ 	this.nombre      = "";
+ 	this.editorial   = "";
+ 	this.autores     = "";
+ 	this.fecha_publi = "";
+ }
